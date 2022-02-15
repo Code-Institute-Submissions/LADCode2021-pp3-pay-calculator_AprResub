f007e441f0e1a943b81c7ec7e9bfc40b4305cb53
LADCode2021 <luke.dawson@live.co.uk>
2022-02-15 16:17:56 +0000

    Create get_dealer_data function to get dealer id from user

--- run.py
+++ run.py
@@ -17,11 +17,40 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('dealer_details')
 
-def clear_work_sheet():
+def clear_worksheet():
     """
     Clears any data in the pay worksheet generated in
     the last use of the pay calculator
     """
     SHEET.worksheet('pay').batch_clear(["B2:D5"])
 
-clear_work_sheet()
+def get_dealer_data():
+    """
+    Some inspiration for this function is taken from get_sales_data()
+    function in the Love Sandwich walkthrough project
+
+    Get dealer id from the user and return name of dealer.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must match a dealer ID in the dealers worksheet.
+    The loop will repeatedly request data, until it is valid.
+    """
+
+    print("Please enter Dealer ID")
+    print("This must match a Dealer ID in the 'dealer' tab")
+    print("Example: 1\n")
+
+    dealer_id = input("Enter Dealer ID here:\n")
+    
+    stored_dealer_id_col = SHEET.worksheet('dealer').col_values(1)
+    stored_dealer_name_col = SHEET.worksheet('dealer').col_values(2)
+    stored_dealer_combine_list = zip(stored_dealer_id_col, stored_dealer_name_col)
+    stored_dealer = dict(stored_dealer_combine_list)
+
+    dealer_name = stored_dealer.get(dealer_id)
+    print(f"You are entering sales data for {dealer_name} with Dealer ID {dealer_id}")
+
+    return dealer_id
+
+
+clear_worksheet()
+get_dealer_data()
